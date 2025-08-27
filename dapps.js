233be0c60c6f0c1bfac9676473583ad6f63c2d80
AylaUzna <Ayla.0xayla@gmail.com>
2025-08-27 18:42:47 +0100

    add dApps

---
+++ dapps.js
@@ -0,0 +1,123 @@
+const DAPPS = [
+  {
+  id: "dapp-000",
+  name: "Intuition Dex",
+  description: "A decentralized exchange built on the Intuition chain.",
+  logoUrl: "images/image.png",    
+  websiteUrl: "https://dex.intuition.box/",
+  category: "DeFi",
+  status: "Launched",
+  tags: ["DEX", "Liquidity"]
+  },
+
+  {
+    id: "dapp-001",
+    name: "Intuition Swap",
+    description: "Faucet Refill: Claim 0.05 tTRUST Every 24 Hours.",
+    logoUrl: "images/faucet.png",
+    websiteUrl: "https://trustfaucet.vercel.app/",
+    category: "DeFi",
+    status: "Launched",
+    tags: ["Faucet"]
+  },
+  {
+    id: "dapp-002",
+    name: "IntuFarm",
+    description: "Maximize Your DeFi Yields: Stake your tokens in our high-yield farming pools and earn competitive rewards. Built on secure smart contracts with transparent, audited protocols..",
+    logoUrl: "images/intuifarm.png",
+    websiteUrl: "https://v0-intu-farm-dapp-farm.vercel.app/",
+    category: "DeFi",
+    status: "Launched",
+    tags: ["DeFi, Dex"]
+  },
+
+   {
+    id: "dapp-003",
+    name: "2048",
+    description: "Join tiles to reach 2048!",
+    logoUrl: "images/2048.png",
+    websiteUrl: "https://2048-intu.vercel.app/",
+    category: "DeFi",
+    status: "Launched",
+    tags: ["Social", "Community"]
+  },
+
+  {
+    id: "dapp-004",
+    name: "Web3 Mood Index",
+    description: "Trust Your Intuition: Navigate crypto markets with confidence using our neural sentiment analysis. Combine data-driven insights with mindful awareness for better decision making.",
+    logoUrl: "images/mood.png",
+    websiteUrl: "https://v0-web3-mood-index-mo.vercel.app/",
+    category: "DeFi",
+    status: "Launched",
+    tags: ["Social", "Community", "DeFi"]
+  },
+
+
+  {
+    id: "dapp-005",
+    name: "TrustSwap",
+    description: "Decentralized Exchange",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://trustswap.vercel.app/",
+    category: "DeFi",
+    status: "In-Progress",
+    tags: ["DeFi", "Community"]
+  },
+  
+  {
+    id: "dapp-006",
+    name: "VitalTip",
+    description: "This decentralized application allows anyone to send small tips to a recipient's wallet address. It's a simple, effective tool to encourage community engagement and reward contributors directly on the blockchain.",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://vitaltip-intuition.vercel.app/",
+    category: "DeFi",
+    status: "Launched",
+    tags: ["Tips", "Community"]
+  },
+  
+  {
+    id: "dapp-005",
+    name: "TrustSwap",
+    description: "Decentralized Exchange",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://trustswap.vercel.app/",
+    category: "DeFi",
+    status: "In-Progress",
+    tags: ["DeFi", "Community"]
+  },
+  
+  {
+    id: "dapp-005",
+    name: "TrustSwap",
+    description: "Decentralized Exchange",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://trustswap.vercel.app/",
+    category: "DeFi",
+    status: "In-Progress",
+    tags: ["DeFi", "Community"]
+  },
+
+  
+  {
+    id: "dapp-005",
+    name: "TrustSwap",
+    description: "Decentralized Exchange",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://trustswap.vercel.app/",
+    category: "DeFi",
+    status: "In-Progress",
+    tags: ["DeFi", "Community"]
+  },
+  
+  {
+    id: "dapp-005",
+    name: "TrustSwap",
+    description: "Decentralized Exchange",
+    logoUrl: "images/trustswap.png",
+    websiteUrl: "https://trustswap.vercel.app/",
+    category: "DeFi",
+    status: "In-Progress",
+    tags: ["DeFi", "Community"]
+  },
+];
